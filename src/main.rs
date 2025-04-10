b912a61c2a1f93c378ccf10a099ade585faadc61
Sharma Anurag <72334404+Anu9969@users.noreply.github.com>
2025-04-10 05:45:03 +0000

    debug macro

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,61 @@
 
-trait Shape{
-  fn area(&self) -> f32;
-}
+// trait Shape{
+//   fn area(&self) -> f32;
+// }
 
-struct Circle{
-  radius: f32
-}
+// struct Circle{
+//   radius: f32
+// }
 
-struct Reactangle{
-  width: u32,
-  height: u32
-}
+// struct Reactangle{
+//   width: u32,
+//   height: u32
+// }
 
-impl Shape for Circle{
-  fn area(&self) -> f32{
-    3.14 *( self.radius * self.radius)
-  }
-}
+// impl Shape for Circle{
+//   fn area(&self) -> f32{
+//     3.14 *( self.radius * self.radius)
+//   }
+// }
 
 
-fn get_area(aaakaar: impl Shape) -> f32{
-    return aaakaar.area()
-}
+// fn get_area(aaakaar: impl Shape) -> f32{
+//     return aaakaar.area()
+// }
 
-fn main() {
-    let c:Circle = Circle{
-        radius: 3.0
-    };
+// fn main() {
+//     let c:Circle = Circle{
+//         radius: 3.0
+//     };
 
-  let r:Reactangle = Rectangle{
-    width:3,
-    height: 4
-  };
-    get_area(c);
+//   let r:Reactangle = Rectangle{
+//     width:3,
+//     height: 4
+//   };
+//     get_area(c);
   
-}
+// }
 
 
 //traits are similar to interfaces in java/javascript
+
+
+//Macros are a way of writing code that writes other code, which is known as metaprogramming.
+#[derive(Debug)]
+
+struct User{
+  name: String,
+  age: u32,
+}
+
+
+fn main() {
+  let u:User = User{
+    name: String::from("Anurag"),
+    age: 21
+  };
+
+  println!("{:?}", u.name);
+  println!("{:?}", u.age);
+  println!("{:?}", u);
+}
